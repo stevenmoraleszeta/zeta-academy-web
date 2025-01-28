477a38a9fcc80adbe9ea78433ce77af872a835eb
Len <107225183+LenVr@users.noreply.github.com>
2025-01-27 20:38:22 -0600

    add feature management functionality in CourseDetail component; implement add, delete, and reorder features with icon editing

--- zeta-academia/src/app/cursos-en-vivo/[courseId]/page.jsx
+++ zeta-academia/src/app/cursos-en-vivo/[courseId]/page.jsx
@@ -121,6 +121,8 @@ const CourseDetail = ({ params }) => {
   const [studentProjects, setStudentProjects] = useState([]);
   const [averageScore, setAverageScore] = useState(0);
   const [displayName, setDisplayName] = useState("");
+  const [editingIconIndex, setEditingIconIndex] = useState(null);
+  const [newIconUrl, setNewIconUrl] = useState("");
 
 
   useEffect(() => {
@@ -458,7 +460,7 @@ const CourseDetail = ({ params }) => {
         updatedProject.studentFileUrl = studentFileUrl;
       }
 
-     
+
 
       // Consulta para encontrar el documento correspondiente al userId
       const studentProjectRef = collection(db, "projects", projectId, "studentsProjects");
@@ -1046,6 +1048,84 @@ const CourseDetail = ({ params }) => {
 
   const averageScoreColor = averageScore >= 80 ? 'blue' : 'red';
 
+  /* Features crud functions */
+
+  const handleAddFeature = async () => {
+    const newFeature = { title: "test", description: "test", iconUrl: 'https://firebasestorage.googleapis.com/v0/b/zeta-3a31d.appspot.com/o/images%2Ficons%2FCertificado%20Icon.png?alt=media&token=608dc368-d510-4276-a551-f50cdcb4b7e6' };
+    try {
+      const courseRef = doc(db, "liveCourses", courseId);
+      await updateDoc(courseRef, {
+        features: arrayUnion(newFeature),
+      });
+
+      console.log("Característica añadida exitosamente");
+    } catch (error) {
+      console.error("Error al agregar la característica:", error);
+    }
+  };
+
+  const handleDeleteFeature = async (index) => {
+    try {
+      const updatedFeatures = [...course.features];
+
+      updatedFeatures.splice(index, 1);
+
+      const courseRef = doc(db, "liveCourses", courseId);
+      await updateDoc(courseRef, { features: updatedFeatures });
+
+      setCourse((prev) => ({
+        ...prev,
+        features: updatedFeatures,
+      }));
+
+      console.log("Característica eliminada exitosamente");
+    } catch (error) {
+      console.error("Error al eliminar la característica:", error);
+    }
+  };
+
+  const moveFeature = async (index, direction) => {
+    setCourse((prevCourse) => {
+      const newFeatures = [...prevCourse.features];
+      const [movedFeature] = newFeatures.splice(index, 1);
+      newFeatures.splice(index + direction, 0, movedFeature);
+
+      // Update the order in the database
+      newFeatures.forEach(async (feature, newIndex) => {
+        try {
+          const courseRef = doc(db, "liveCourses", courseId);
+          await updateDoc(courseRef, { features: newFeatures });
+        } catch (error) {
+          console.error("Error updating feature order:", error);
+        }
+      });
+
+      return { ...prevCourse, features: newFeatures };
+    });
+  };
+
+  const handleIconClick = (index) => {
+    setEditingIconIndex(index);
+    setNewIconUrl(course.features[index].iconUrl);
+  };
+
+  const handleIconFeatureChange = (e) => {
+    setNewIconUrl(e.target.value);
+  };
+
+  const saveIconUrl = async (index) => {
+    const updatedFeatures = [...course.features];
+    updatedFeatures[index].iconUrl = newIconUrl;
+
+    try {
+      const courseRef = doc(db, "liveCourses", courseId);
+      await updateDoc(courseRef, { features: updatedFeatures });
+      setCourse((prev) => ({ ...prev, features: updatedFeatures }));
+      setEditingIconIndex(null);
+    } catch (error) {
+      console.error("Error updating icon URL:", error);
+    }
+  };
 
   return (
     <div className={styles.container}>
@@ -1168,9 +1248,19 @@ const CourseDetail = ({ params }) => {
       </div>
 
       <div className={styles.features}>
-        {(course.features || []).map((feature, index) => (
+        {isAdmin && (
+          <>
+            <div className={styles.actionBtnsContainer}>
+              <button onClick={handleAddFeature} className={styles.featuresActionsBtn}>
+                <FaPlus />
+              </button>
+            </div>
+            <div></div>
+          </>
+        )}
+        {(course.features || defaultFeatures).map((feature, index) => (
           <div key={index} className={styles.feature}>
-            <div className={styles.featureIcon}>
+            <div className={styles.featureIcon} onClick={() => handleIconClick(index)}>
               <Image
                 src={feature.iconUrl}
                 alt={`Icono de ${feature.title}`}
@@ -1178,6 +1268,19 @@ const CourseDetail = ({ params }) => {
                 style={{ objectFit: "contain" }} // Ajusta según cómo quieras que se muestren los íconos
               />
             </div>
+            {editingIconIndex === index && (
+              <div className={styles.iconUrlInputContainer}>
+                <input
+                  type="text"
+                  value={newIconUrl}
+                  onChange={handleIconFeatureChange}
+                  className={styles.iconUrlInput}
+                />
+                <button onClick={() => saveIconUrl(index)} className={styles.saveButton}>
+                  Guardar
+                </button>
+              </div>
+            )}
             <div>
               {isAdmin ? (
                 <>
@@ -1204,10 +1307,40 @@ const CourseDetail = ({ params }) => {
               ) : (
                 <>
                   <div className={styles.featureTitleInput}>
-                    {feature.title || "Sin título"}
+                    {feature.title || "Título no disponible"}
                   </div>
                   <div className={styles.featureDescriptionInput}>
-                    {feature.description || "Sin descripción"}
+                    {feature.description || "Descripción no disponible"}
+                  </div>
+                </>
+              )}
+              {isAdmin && (
+                <>
+                  <div className={styles.featuresActionsContainer}>
+                    <div className={styles.featureActions}>
+                      <button
+                        onClick={() => moveFeature(index, -1)}
+                        disabled={index === 0}
+                        className={styles.moveButton}
+                      >
+                        <FaArrowUp />
+                      </button>
+                      <button
+                        onClick={() => moveFeature(index, 1)}
+                        disabled={index === course.features.length - 1}
+                        className={styles.moveButton}
+                      >
+                        <FaArrowDown />
+                      </button>
+                    </div>
+                    <div>
+                      <button
+                        className={styles.featuresActionsBtn}
+                        onClick={() => handleDeleteFeature(index)}
+                      >
+                        <FaTrash />
+                      </button>
+                    </div>
                   </div>
                 </>
               )}
