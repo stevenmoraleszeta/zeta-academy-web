72690cd716c318121914dd172f4ac0ec77f13693
Len <107225183+LenVr@users.noreply.github.com>
2025-01-29 13:56:13 -0600

    Improve add feature funct, there is no need to refresh the website after adding a feature

--- zeta-academia/src/app/cursos-en-linea/[courseId]/page.jsx
+++ zeta-academia/src/app/cursos-en-linea/[courseId]/page.jsx
@@ -586,6 +586,11 @@ const CourseDetail = ({ params }) => {
         features: arrayUnion(newFeature),
       });
 
+      setCourse((prev) => ({
+        ...prev,
+        features: [...(prev.features || []), newFeature],
+      }));
+
       console.log("Característica añadida exitosamente");
     } catch (error) {
       console.error("Error al agregar la característica:", error);
