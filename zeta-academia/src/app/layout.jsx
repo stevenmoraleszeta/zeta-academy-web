5a0112ea3a5210e1418c9c325ef7fe0b416511f9
Len <107225183+LenVr@users.noreply.github.com>
2025-02-13 16:52:25 -0600

    Remove unused favicon files and update layout with new favicon links and Open Graph images for improved branding

--- zeta-academia/src/app/layout.jsx
+++ zeta-academia/src/app/layout.jsx
@@ -22,16 +22,26 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon.png" media="(prefers-color-scheme: dark)" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/faviconBlk.jpg" media="(prefers-color-scheme: light)" />
         <link rel="icon" href="/favicon.ico" />
-        <link rel="icon" type="image/png" sizes="32x32" href="/faviconWeb.png" />
 
         <meta property="og:title" content="ZETA Academia" />
         <meta property="og:description" content="ZETA Plataforma Educativa" />
-        <meta property="og:image" content="https://firebasestorage.googleapis.com/v0/b/zeta-3a31d.appspot.com/o/images%2Ficons%2FZ%201200.png?alt=media&token=1dfe7287-6ea0-4ca2-aa3b-25dc9af60b98" />
-        <meta property="og:image:width" content="1200" />
-        <meta property="og:image:height" content="630" />
         <meta property="og:url" content="https://zetaacademia.com" />
         <meta property="og:type" content="website" />
+
+        <meta property="og:image" content="https://firebasestorage.googleapis.com/v0/b/zeta-3a31d.appspot.com/o/images%2Ficons%2FZETA%201200.png?alt=media&token=3adb303b-a52f-4f7f-8266-b2bbba867083" />
+        <meta property="og:image:width" content="1200" />
+        <meta property="og:image:height" content="630" />
+
+        <meta property="og:image" content="https://firebasestorage.googleapis.com/v0/b/zeta-3a31d.appspot.com/o/images%2Ficons%2FZ%201200.png?alt=media&token=1dfe7287-6ea0-4ca2-aa3b-25dc9af60b98" />
+        <meta property="og:image:width" content="500" />
+        <meta property="og:image:height" content="500" />
+
+        <meta property="og:image" content="https://firebasestorage.googleapis.com/v0/b/zeta-3a31d.appspot.com/o/images%2Ficons%2FZ%201200.png?alt=media&token=1dfe7287-6ea0-4ca2-aa3b-25dc9af60b98" />
+        <meta property="og:image:width" content="200" />
+        <meta property="og:image:height" content="200" />
       </head>
       <body className={`app ${montserrat.className}`}>
         <AuthProvider>
